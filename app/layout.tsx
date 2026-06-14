3a198f441ca4c46e631d53892f176a1ec7726f3a
robertk-star <robertk@ardykay.com>
2026-06-14 08:08:42 -0500

    Add app layout

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./globals.css";
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return React.createElement(
+    "html",
+    { lang: "en" },
+    React.createElement("body", null, props.children)
+  );
+}
